58d55c26b5ef4aee85884cee1fe3628a64a831c9
MaksGorobets <gorobets542@gmail.com>
2024-01-10 00:16:41 +0200

    Initial Commit

---
+++ TestingApp/TestingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingAppApp.swift
+//  TestingApp
+//
+//  Created by Maks Winters on 10.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
